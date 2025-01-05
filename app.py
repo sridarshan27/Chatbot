1122aabeab26a20287fc05e049763b6e49c00795
Sri Darshan <sridarshan2709@gmail.com>
2025-01-05 22:23:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ clf.fit(x, y)
 def chatbot(input_text):
     input_text = vectorizer.transform([input_text])
     tag = clf.predict(input_text)[0]
-    for intent in intents:
+   for intent in intents['intents']:
         if intent['tag'] == tag:
             response = random.choice(intent['responses'])
             return response
